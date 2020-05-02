c981e5d0246cd264355ace7ac79dabaed810f840
Jack Hou <jackhou1998@gmail.com>
2020-05-02 14:35:05 -0700

    monthly challenges folder

---
+++ Monthly_Challenges/may2_2020_test.py
@@ -0,0 +1,21 @@
+import unittest
+from may2_2020 import Solution
+
+class testSolution(unittest.TestCase): 
+    global sol
+    sol = Solution()
+
+    def test(self): 
+        self.assertEqual(sol.numJewelsInStones('aA','aAAbbbb'), 3)
+        self.assertEqual(sol.numJewelsInStones('z','ZZ'), 0)
+        self.assertEqual(sol.numJewelsInStones('','A'),0)
+        self.assertEqual(sol.numJewelsInStones('A',''),0)
+        self.assertEqual(sol.numJewelsInStones('',''),0)
+        self.assertEqual(sol.numJewelsInStones('',''),0)
+        self.assertEqual(sol.numJewelsInStones('z','aaaaaaaaaaz'),1)
+        self.assertEqual(sol.numJewelsInStones('x','xzzzzzzzzzzzz'),1)
+        self.assertEqual(sol.numJewelsInStones('ebd','bbb'),3)
+
+if __name__ == '__main__':
+    unit_test = testSolution()
+    unit_test.test()
